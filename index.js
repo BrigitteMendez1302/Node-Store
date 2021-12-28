4eee5afbbc8bdc5dbb829a0a89ffb4911ebd1484
Brigitte <u201914955@upc.edu.pe>
2021-12-28 16:07:18 -0500

    add: class 23

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes')
 
 const {logErrors, errorHandler, boomErrorHandler} = require('./middlewares/error.hendler.')
@@ -8,12 +9,30 @@ const port = 3000;
 
 app.use(express.json());
 
+const whitelist = ['http://localhost:8080', 'https://myapp.com']
+const options = {
+  origin: (origin, callback)=>{
+    if (whitelist.includes(origin)){
+      callback(null, true)
+    }else{
+      callback(new Error('No permitido'))
+    }
+  }
+}
+app.use(cors(options))
+
 routerApi(app);
 
 app.use(logErrors)
 app.use(boomErrorHandler)
 app.use(errorHandler)
-
+const swaggerUi = require('swagger-ui-express'),
+swaggerDocument = require('./swagger.json');
+app.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument)
+);
 app.listen(port, ()=>{
   console.log("Estoy corriendo")
 })
